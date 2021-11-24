96150e059513b88fbfee1886473d4a2e3ae5bfa3
Gagan Agarwal <53825001+GaganAgarwal77@users.noreply.github.com>
2021-11-24 18:39:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,16 @@
 import unittest
 from program import factorial
 
-class TestFactorial(unittest.TestCase):
+class TestMean(unittest.TestCase):
     def test_mul1(self):
         result1 = factorial(5)
         self.assertEqual(result1, 120)
     def test_mul2(self):
         result2 = factorial(0)
-        self.assertEqual(result2, 1)
+        self.assertEqual(result2, 0)
     def test_mul3(self):
         result3 = factorial(10)
-        self.assertEqual(result3, 3628800)
+        self.assertEqual(result3, 1)
 
 
 if __name__ == '__main__':
